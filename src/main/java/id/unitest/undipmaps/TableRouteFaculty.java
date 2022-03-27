240682bb876b5192aded6d64c715d421db4268bf
Samuel Batara <samuelbatara1307@gmail.com>
2022-03-27 18:11:40 +0700

    Remove indexes attribute

--- src/main/java/id/unitest/undipmaps/TableRouteFaculty.java
+++ src/main/java/id/unitest/undipmaps/TableRouteFaculty.java
@@ -1,67 +1,43 @@
 package id.unitest.undipmaps;
 
-import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class TableRouteFaculty {
-	private HashMap<String, Integer> indexes;
+public class TableRouteFaculty { 
 	private ArrayList<ArrayList<Integer>> routes;
 	private ArrayList<String> faculties;
 	
 	public TableRouteFaculty() {
 		// Add faculty
 		faculties = new ArrayList<String>();
-		faculties.add("FSM");
-		faculties.add("FEB");
-		faculties.add("FP");
-		faculties.add("FT");
-		faculties.add("FPP");
-		faculties.add("FPIK");
-		faculties.add("FKM");
-		faculties.add("FK");
-		faculties.add("FIB");
-		faculties.add("FISIP");
-		faculties.add("FH");
-		faculties.add("SV");
+		faculties.add("FSM"); faculties.add("FEB"); faculties.add("FP");
+		faculties.add("FT"); faculties.add("FPP"); faculties.add("FPIK");
+		faculties.add("FKM"); faculties.add("FK"); faculties.add("FIB");
+		faculties.add("FISIP"); faculties.add("FH"); faculties.add("SV");
 		
-		// Add index and abbreviation of faculty 
-		indexes = new HashMap<String, Integer>();
-		indexes.put("FSM", 0);
-		indexes.put("FEB", 1);
-		indexes.put("FP", 2);
-		indexes.put("FT", 3);
-		indexes.put("FPP", 4);
-		indexes.put("FPIK", 5);
-		indexes.put("FKM", 6);
-		indexes.put("FK", 7);
-		indexes.put("FIB", 8);
-		indexes.put("FISIP", 9);
-		indexes.put("FH", 10);
-		indexes.put("SV", 11);
 		
 		// Add route between faculties
 		routes = new ArrayList<ArrayList<Integer>>();
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FT"), indexes.get("FPP"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FT"), indexes.get("FSM"), 10) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FT"), indexes.get("FISIP"), 15) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FPP"), indexes.get("FPIK"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FPIK"), indexes.get("FKM"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FKM"), indexes.get("FEB"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FKM"), indexes.get("FK"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FK"), indexes.get("FEB"), 15) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FK"), indexes.get("FP"), 25) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FEB"), indexes.get("FP"), 10) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FSM"), indexes.get("FEB"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FSM"), indexes.get("FP"), 15) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FP"), indexes.get("FIB"), 50) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FIB"), indexes.get("FSM"), 14) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FIB"), indexes.get("SV"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FH"), indexes.get("SV"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FH"), indexes.get("FIB"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FH"), indexes.get("FSM"), 15) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FISIP"), indexes.get("FH"), 5) ));
-		routes.add(new ArrayList<Integer>(Arrays.asList(indexes.get("FISIP"), indexes.get("FSM"), 17) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FT"), faculties.indexOf("FPP"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FT"), faculties.indexOf("FSM"), 10) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FT"), faculties.indexOf("FISIP"), 15) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FPP"), faculties.indexOf("FPIK"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FPIK"), faculties.indexOf("FKM"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FKM"), faculties.indexOf("FEB"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FKM"), faculties.indexOf("FK"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FK"), faculties.indexOf("FEB"), 15) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FK"), faculties.indexOf("FP"), 25) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FEB"), faculties.indexOf("FP"), 10) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FSM"), faculties.indexOf("FEB"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FSM"), faculties.indexOf("FP"), 15) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FP"), faculties.indexOf("FIB"), 50) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FIB"), faculties.indexOf("FSM"), 14) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FIB"), faculties.indexOf("SV"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FH"), faculties.indexOf("SV"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FH"), faculties.indexOf("FIB"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FH"), faculties.indexOf("FSM"), 15) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FISIP"), faculties.indexOf("FH"), 5) ));
+		routes.add(new ArrayList<Integer>(Arrays.asList(faculties.indexOf("FISIP"), faculties.indexOf("FSM"), 17) ));
 	}
 	
 	/* Samuel Batara
@@ -79,7 +55,7 @@ public class TableRouteFaculty {
 	 * @return Integer 
 	 */
 	public Integer getIndex(String faculty) {
-		return indexes.get(faculty);
+		return faculties.indexOf(faculty);
 	}
 	
 	/* Samuel Batara
